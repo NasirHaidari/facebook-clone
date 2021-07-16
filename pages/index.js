8f17f69a7f7cb2ec40e07387d4a427801f58962d
NasirHaidari <nasir.haidri@htomail.com>
2021-07-16 03:20:39 +0200

    the app is working 100%

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,10 @@ import Feed from '../components/Feed'
 import Header from '../components/Header'
 import Login from '../components/Login'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import { db } from '../firebase'
 
-export default function Home({ session }) {
+export default function Home({ session, posts }) {
   if (!session) return <Login />
   return (
     <div className='h-screen bg-gray-100 overflow-hidden'>
@@ -15,7 +17,8 @@ export default function Home({ session }) {
       <Header />
       <main className='flex'>
         <Sidebar />
-        <Feed />
+        <Feed posts={posts} />
+        <Widgets />
       </main>
     </div>
   )
@@ -23,9 +26,19 @@ export default function Home({ session }) {
 
 export async function getServerSideProps(context) {
   const session = await getSession(context)
+
+  const posts = await db.collection('post').orderBy('timestamp', 'desc').get()
+
+  const docs = posts.docs.map((post) => ({
+    id: post.id,
+    ...post.data(),
+    timestamp: null,
+  }))
+
   return {
     props: {
       session,
+      posts: docs,
     },
   }
 }
